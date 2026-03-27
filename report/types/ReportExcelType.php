adcb14f17635113d1058032aab0a083d67aa9c04
Nasirov Ismaiil <nasirov-ismaiil@yandex.ru>
2026-03-27 11:19:07 +0300

    project init

---
+++ report/types/ReportExcelType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace report\types;
+
+use report\ReportInterface;
+
+/**
+ * Реализация генерации отчета формата xlsx
+ *
+ * @class ExcelType
+ */
+class ExcelType implements ReportInterface
+{
+
+}
